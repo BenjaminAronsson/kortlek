e8056937e2775510ec06059cd726b636a3a47bbd
dev <bene.a99@gmail.com>
2019-04-05 09:35:18 +0200

    added cards and deck models

---
+++ kortlek/Model/Card.swift
@@ -0,0 +1,14 @@
+//
+//  Card.swift
+//  kortlek
+//
+//  Created by Benjamin on 2019-04-05.
+//  Copyright © 2019 Benjamin. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    let suite : String
+    let rank : String
+}
---
+++ kortlek/Model/Deck.swift
@@ -0,0 +1,17 @@
+//
+//  Deck.swift
+//  kortlek
+//
+//  Created by Benjamin on 2019-04-05.
+//  Copyright © 2019 Benjamin. All rights reserved.
+//
+
+import Foundation
+
+class Deck {
+    var cards = [Card]()
+    
+    let ranksss = ["heart", "diamond", ""]
+    let suites = ["♥", "♠", "♦", "♣"]
+    let ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+}
