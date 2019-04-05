951499f41291968651e3ae7fdcffb853cbfe5560
dev <bene.a99@gmail.com>
2019-04-05 10:26:00 +0200

    adding button to screen

--- kortlek/Controller/ViewController.swift
+++ kortlek/Controller/ViewController.swift
@@ -10,12 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let cardButton : UIButton = {
+        let button = UIButton(frame: .zero)
+        button.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let deck = Deck()
         
+        setupUI()
+    }
+    
+    func setupUI() {
+        //UIScreen.main.bounds
+        let size = view.frame.size
+        let witdh = size.width
+        let height = size.height
+        
+        let frame = CGRect(x: witdh / 2 - 75, y: height / 2 - 25, width: 150, height: 50)
+        cardButton.frame = frame
+        cardButton.addTarget(self, action: #selector(nextButtonPressed), for: .touchUpInside)
+        
+        view.addSubview(cardButton)
     }
 
-
+    @objc func nextButtonPressed(_ sender : UIButton) {
+        
+    }
+    
 }
 
--- kortlek/Model/Deck.swift
+++ kortlek/Model/Deck.swift
@@ -14,4 +14,27 @@ class Deck {
     let ranksss = ["heart", "diamond", ""]
     let suites = ["♥", "♠", "♦", "♣"]
     let ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    
+    init() {
+        newDeck()
+    }
+    
+    //creates new deck
+    func newDeck() {
+        for suite in suites {
+            for rank in ranks {
+                let newCard = Card(suite: suite, rank: rank)
+                cards.append(newCard)
+            }
+        }
+    }
+    
+    func drawTopCard() -> Card {
+        let topCard = cards.removeFirst()
+        return topCard
+    }
+    
+    func shuffleCard() {
+        
+    }
 }
